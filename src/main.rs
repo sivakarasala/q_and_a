dac383a12ad9bfd42a89322542ad575264c88e55
Siva Karasala <shivataraya@gmail.com>
2025-12-13 11:46:50 +0530

    feat: setup cors

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-use std::str::FromStr;
 use std::io::{Error, ErrorKind};
+use std::str::FromStr;
 
 use serde::Serialize;
-use warp::{Filter, reject::Reject, Rejection, Reply, http::StatusCode};
+use warp::{
+    Filter, Rejection, Reply, filters::cors::CorsForbidden, http::Method, http::StatusCode,
+    reject::Reject,
+};
 
 #[derive(Debug, Serialize)]
 struct Question {
@@ -16,12 +19,7 @@ struct Question {
 struct QuestionId(String);
 
 impl Question {
-    fn new(
-        id: QuestionId,
-        title: String,
-        content: String,
-        tags: Option<Vec<String>>,
-    ) -> Self {
+    fn new(id: QuestionId, title: String, content: String, tags: Option<Vec<String>>) -> Self {
         Self {
             id,
             title,
@@ -51,27 +49,29 @@ async fn get_questions() -> Result<impl warp::Reply, warp::Rejection> {
         QuestionId::from_str("1").expect("No id provided"),
         "First Question".to_string(),
         "Content of question".to_string(),
-        Some(vec!("faq".to_string())),
+        Some(vec!["faq".to_string()]),
     );
-    
+
     match question.id.0.parse::<i32>() {
-        Err(_) => {
-            Err(warp::reject::custom(InvalidId))
-        },
-        Ok(_) => Ok(warp::reply::json(&question))
+        Err(_) => Err(warp::reject::custom(InvalidId)),
+        Ok(_) => Ok(warp::reply::json(&question)),
     }
-    
 }
 
 async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
-    if let Some(InvalidId) = r.find() {
+    if let Some(error) = r.find::<CorsForbidden>() {
         Ok(warp::reply::with_status(
-            "No valid ID presented",
+            error.to_string(),
+            StatusCode::FORBIDDEN,
+        ))
+    } else if let Some(InvalidId) = r.find() {
+        Ok(warp::reply::with_status(
+            "No valid ID presented".to_string(),
             StatusCode::UNPROCESSABLE_ENTITY,
         ))
     } else {
         Ok(warp::reply::with_status(
-            "Route not found",
+            "Route not found".to_string(),
             StatusCode::NOT_FOUND,
         ))
     }
@@ -79,13 +79,17 @@ async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
 
 #[tokio::main]
 async fn main() {
+    let cors = warp::cors()
+        .allow_any_origin()
+        .allow_header("content-type")
+        .allow_methods(&[Method::PUT, Method::DELETE, Method::GET, Method::POST]);
     let get_items = warp::get()
         .and(warp::path("questions"))
         .and(warp::path::end())
         .and_then(get_questions)
         .recover(return_error);
 
-    let routes = get_items;
+    let routes = get_items.with(cors);
 
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
